a465d54c60ece538ad4d02bb410a45f30ad3f53f
Rarroyo941 <arroyo.rodrigo@outlook.com>
2023-02-05 23:35:36 -0300

    second commit

--- Portfolio/src/component/Main/Main.jsx
+++ Portfolio/src/component/Main/Main.jsx
@@ -1,25 +1,42 @@
-import './Main.css';
-import fotografo from '../../imagenes/fotografo.jpg';
+import './Main.css'
+import fotografo from '../../imagenes/fotografo.jpg'
 import mujer from '../../imagenes/mujer.jpg'
 import arbol from '../../imagenes/arbol.jpg'
-
-
+import cataratas from  '../../imagenes/cataratas.jpg'
+import asado from '../../imagenes/asado.jpg'
+import bandera from '../../imagenes/bandera.jpg'
+import glaciar from '../../imagenes/glaciar.jpg'
+import montania from '../../imagenes/montania.jpg'
+import ninio from '../../imagenes/ninio.jpg'
+import obelisco from '../../imagenes/obelisco.jpg'
+import mujer2 from '../../imagenes/mujer2.jpg'
 const Main =()=>{ 
     return (
       <>
-      <div className='main'>
+      <section className='main'>
         <img src={fotografo} alt="fotografo" className='fotografo'  />
         <img src={mujer} alt="mujer" className='mujer'/>
         <img src={arbol} alt="arbol" className='arbol'/>
-      </div>
+      </section>
       <div className="about">
         <h2>Sobre mi</h2>
         <p>En 2017 decidí estudiar la carrera de Licenciatura en Fotografia en la Universidad Nacional de San Martín, graduandome en 2020. Ahora resido en la ciudad de Rosario, dedicandome exclusivamente a la fotografía de la naturaleza.</p>
       </div>
-      <div className="contacto">
+      <section className="contacto">
         <h1>Capturá los mejores momentos de tu vida</h1>
         <button>CONTACTO</button>
-      </div>
+      </section>
+      <section className='galeria'>
+        <div className="grid-item"><img src={bandera} alt="bandera"/></div>
+        <div className='grid-item'><p>Portfolio</p></div>
+        <div className="grid-item"><img src={cataratas} alt="cataratas"/></div>
+        <div className="grid-item"><img src={glaciar} alt="glaciar"/></div>
+        <div className="grid-item"><img src={mujer2} alt="mujer"/></div>
+        <div className="grid-item"><img src={ninio} alt="ninio"/></div>
+        <div className="grid-item"><img src={montania} alt="montania"/></div>
+        <div className="grid-item"><img src={obelisco} alt="obelisco"/></div>
+        <div className="grid-item"><img src={asado} alt="asado"/></div>
+      </section>
       </>
     )
 }
